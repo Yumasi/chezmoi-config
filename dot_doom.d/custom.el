a465309abde200fdbded7945a34c87ee1428d9f3
Guillaume Pagnoux <gpagnoux@gmail.com>
2022-09-05 14:50:22 +0200

    add Doom config
    Signed-off-by: Guillaume Pagnoux <gpagnoux@gmail.com>

---
+++ dot_doom.d/custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("6f4421bf31387397f6710b6f6381c448d1a71944d9e9da4e0057b3fe5d6f2fad" default))
+ '(safe-local-variable-values
+   '((lsp-go-env
+      (GOOS . "linux"))
+     (lsp-go-build-tags .
+                        ["jetson" "zk" "etcd" "clusterchecks" "cri" "linux_bpf" "jmx" "containerd" "orchestrator" "systemd" "ec2" "docker" "secrets" "zlib" "otlp" "process" "gce" "netcgo" "kubeapiserver" "consul" "python" "apm" "podman" "kubelet" "npm"])
+     (lsp-go-build-tags .
+                        ["jmx" "consul" "docker" "python" "systemd" "ec2" "gce" "zk" "containerd" "cri" "apm" "etcd" "kubeapiserver" "podman" "process" "netcgo" "otlp" "orchestrator" "kubelet" "zlib" "secrets" "jetson"]))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
